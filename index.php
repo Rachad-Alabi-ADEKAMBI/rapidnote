bbec8a5eff5c41536d1c7ebb6032fcfd7647c04f
Rachad ADEKAMBI <adekambirachad@gmail.com>
2022-04-10 17:37:03 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include 'functions.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'meta.php' ?>
+
+    <title>Rapid Note - Achat et Revente de vos cryptomonnaies en quelques clics</title>
+</head>
+<body>
+    <?php include 'header.php' ?>
+    
+    <main class="main">
+        <div class="top">
+
+        </div>
+    </main>
+
+
+    
+    <?php include 'footer.php'; ?>
+</body>
+</html>
