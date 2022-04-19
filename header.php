7cd518b9e74bec712013809177a588bbf3ff1e24
Rachad ADEKAMBI <adekambirachad@gmail.com>
2022-04-19 21:10:18 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,45 @@
+<nav class="nav">
+    <a href="index.php" class="nav__logo">
+        <img src="public/images/logo.png" alt="">
+    </a>
+
+    <ul class="nav__menu">
+        <li>
+            <a href="">Comment ca marche ?</a>
+        </li>
+
+        <li>
+            <a href="">
+                Acheter
+            </a>
+        </li>
+
+        <li>
+            <a href="">
+                Vendre
+            </a>
+        </li>
+    </ul>
+
+    <div class="nav__login">
+        <ul class="nav__login__link">
+        <li>
+                <button>
+                <a href="" >
+                    Inscription
+                </a>
+                </button>
+            </li>
+            
+            <li>
+                <button>
+                <a href="" class='login-link'>
+                    Connexion
+                </a>
+                </button>
+            </li>
+
+           
+        </ul>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-<?php include 'functions.php'; ?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <?php include 'meta.php' ?>
 
-    <title>Rapid Note - Achat et Revente de vos cryptomonnaies en quelques clics</title>
+<head>
+    <?php include 'meta.php'; ?>   
+
+    <title>Rapid Note - Achat et revente de crypto au Ghna</title>
 </head>
 <body>
     <?php include 'header.php' ?>
@@ -13,6 +12,207 @@
     <main class="main">
         <div class="top">
 
+            <div class="top__text">
+                <h1 class="top__text__title">
+                    RAPID TRANSFER
+                </h1>
+
+                <p class="top__text__text">
+                    Nous vous échangeons vos crypto-actifs en quelques clics
+                </p>
+
+                <h2 class="top__text__subtitle">
+                    Notre plateforme vous permet d'échanger Lorem ipsum dolor sit, 
+                    amet consectetur adipisicing elit. Ea laudantium totam
+                     minima dolore enim. Id, numquam blanditiis perferendis
+                </h2>
+
+                <ul class="top__text__items">
+                    <li>
+                        Facile à utiliser
+                    </li>
+
+                    <li>
+                        Sécurisé
+                    </li>
+
+                    <li>
+                        Vous etes rémunéré pour vos opérations
+                    </li>
+                </ul>
+            </div>
+            <div class="top__image">
+                <div class="top__image__box">
+                    <img src="public/images/bitcoin.jpg" alt="">
+                    <img src="public/images/ghc.jpg" alt="">
+                </div>
+            </div>
+            
+        </div>
+
+        <div class="reasons">
+            <h2 class="reasons__title">
+                Comment ca marche ?
+            </h2>
+            <div class="reasons__sections">
+                <div class="reasons__sections__box">
+                    <div class="box-img">
+                        <p>1</p>
+                    </div>
+
+                    <p>
+                        <a href="">Connectez vous</a> à votre compte
+                    </p>
+
+
+                </div>
+
+                <div class="reasons__sections__box">
+                    <div class="box-img">
+                        2
+                    </div>
+
+                    <h4>
+                        Choisissez <a href="">Acheter</a> ou <a href="">Vendre </a>
+                    </h4>
+                </div>
+
+                <div class="reasons__sections__box">
+                    <div class="box-img">
+                        3
+                    </div>
+
+                    <h4>
+                      Le solde est transféré sur votre portefeuille  Mobile money ou Bitcoin
+                    </h4>
+
+
+                </div>
+
+               
+            </div>
+        </div>
+
+        <div class="simulator">
+            <img src="" alt="" class="simulator__image">
+            <h3 class="simulator__title">
+                Et si vous faisiez une simulation ?
+            </h3>
+
+            <p class="simulator__text">
+                Découvrez en quelques clics nos solutions
+            </p>
+
+            <div class="simulator__buttons">
+                    <form action="">
+                        <button> Simuler achat</button>
+                    </form>
+
+                    <form action="">
+                        <button> Simuler vente</button>
+                    </form>
+                </div>
+        </div>
+
+            <div class="about">
+               <div class="about__left">
+                    <h2 class="about__left__title">
+                            Pourquoi nous choisir ?
+                        </h2>
+
+                        <p class="about__left__text">
+                        Nous sommes convaincus  que garantir la sécurité et le un faible taux de change 
+                        des devises commes le bitcoin et le ghc sont des mesures à prendre en considération 
+                        pour le bien etre de l'econonmie et des citoyens. Notre entreprise vous accompagne dans 
+                        l'échange des devises précitées tout en vous promettant ces deux garanties. Plusieurs autres
+                        avantages vous attendent...
+                        </p> 
+
+                        <button class="about__left__btn">
+                            En savoir plus
+                        </button>
+               </div>
+
+               <div class="about__img">
+                   <img src="public/images/statue-accra.jpg" alt="">
+               </div>
+            </div>
+
+        <div class="care">
+            <div class="card">
+                <div class="card__icon">
+                    <i class="fas fa-headset"></i>
+                </div>
+                    <div class="card__text">
+                        <h3 class="card__text__title">
+                            Nous sommes à votre écoute
+                        </h3>
+
+                        <p class="card__text__text">
+                            <span>Contactez-nous</span> <br>
+
+                            Quel que soit votre préoccupation, notre équipe disponible
+                            24/7 vous aidera dans la marche à suivre
+                        </p>
+
+                        <button>
+                            Contact
+                        </button>
+                    </div>
+            </div>
+
+            <div class="card">
+                    
+            <div class="card__icon">
+                    <i class="fas fa-headset"></i>
+                </div>
+                    <div class="card__text">
+                        <h3 class="card__text__title">
+                            Nous sommes à votre écoute
+                        </h3>
+
+                        <p class="card__text__text">
+                            <span>Vous etes rémunéré</span> <br>
+
+                        Vous appréciez notre service ? Parlez-en à votre entourege et gagnez 10%
+                        de la 1ere transaction de votre filleul. Votre interet est également le notre.
+                        </p>
+
+                        <button>
+                            Contact
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+       
+
+        <?php include 'graph.php'; ?>
+
+        <div class="payments">
+            <h2 class="payments__title">
+                Nos différents moyens de paiement
+            </h2>
+
+            <div class="payments__items">
+                <div class="payments__items__item">
+                <img src="public/images/img-mtn.png" alt="">
+                </div>
+                <div class="payments__items__item">`
+                    <img src="public/images/mastercard.png" alt="">
+                </div>
+                <div class="payments__items__item">`
+                    <img src="public/images/visa.png" alt="">
+                </div>
+                <div class="payments__items__item">`
+                    <img src="public/images/paypal-rond.png" alt="">
+                </div>
+                <div class="payments__items__item">`
+                    <img src="public/images/btc.png" alt="">
+                </div>
+            </div>
         </div>
     </main>
 
